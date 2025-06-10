8ba72b130ccc4dda3bf56f42b45f4a366cd4a90a
Tanmay312007 <shahtanmay2007@gmail.com>
2025-06-10 20:26:08 +0530

    loop

---
+++ arrays/loops.js
@@ -0,0 +1,20 @@
+let endnumber = 10;
+let num = 1;
+while (num <= endnumber) {
+  console.log(num);
+  num++;
+}
+console.log();
+
+let lastnumber = 1;
+let numb = 10;
+while (numb >= lastnumber) {
+  console.log(numb);
+  numb--;
+}
+let end = 10;
+let n = 1;
+while (n <= end) {
+  console.log(`2 * ${n} = ${2 * n}`);
+  n++;
+}
